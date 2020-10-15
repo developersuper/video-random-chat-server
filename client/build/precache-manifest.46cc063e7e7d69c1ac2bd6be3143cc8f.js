eb51c58d68f3b0a2b16d2bf040588d38afc2bb63
nikita <kolikhanovn@mail.ru>
2020-10-15 07:16:40 -0700

    update for one server

---
+++ client/build/precache-manifest.46cc063e7e7d69c1ac2bd6be3143cc8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eedb5d397e746b7c77165a2aa9cfe52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a02dff85275924427d4",
+    "url": "/static/css/2.fdf5bcfc.chunk.css"
+  },
+  {
+    "revision": "2eb7e97169011196d9eb",
+    "url": "/static/css/main.a2ce4c00.chunk.css"
+  },
+  {
+    "revision": "4a02dff85275924427d4",
+    "url": "/static/js/2.b7a73881.chunk.js"
+  },
+  {
+    "revision": "fb1c7a748795ff408cb98abe4a5d86b2",
+    "url": "/static/js/2.b7a73881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb7e97169011196d9eb",
+    "url": "/static/js/main.fdf17656.chunk.js"
+  },
+  {
+    "revision": "44aee220bccf4ace3598",
+    "url": "/static/js/runtime-main.d4e61eff.js"
+  }
+]);
